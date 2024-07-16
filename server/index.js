5c6cdd8a357b33b768b07f6084386710108e1d11
Anjay <rajputanjay@gmail.com>
2024-07-16 10:32:30 +0530

    fix cors

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ app.get('/', (req, res) => {
 })
 
 const corsOptions = {
-  origin: 'https://portfolio-anjay29.vercel.app',
+  origin: ['https://portfolio-anjay29.vercel.app',
+    'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
